eecb3fec6a7c9a8ea56eefb874f3023e93765c42
Dario Guidi <dario.guidi954@gmail.com>
2025-03-29 23:18:13 +0100

    .

--- Laboratorio 1/Esercizio-1.c
+++ Laboratorio 1/Esercizio-1.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main() {
     int N1, N2;
-    float risp;
+    float ris;
     char op;
 
     printf("Inserisci calcolo nella forma <n1> <op> <n2>\n");
-    if(scanf("%d %c %d", N1, op, N2) != 3){
-        printf("Si vede che hai sbagliato qualche cosa");
+    if (scanf("%d %c %d", &N1, &op, &N2) != 3) {
+        printf("Errore: input non valido.\n");
         return 1;
     }
 
-    switch(op){
+    switch (op) {
         case '+':
-            risp=N1+N2;
-            printf("Il risultato risulta: %d", risp);
+            ris = N1 + N2;
+            printf("Risultato: %.2f\n", ris);
+            break;
+        case '-':
+            ris = N1 - N2;
+            printf("Risultato: %.2f\n", ris);
+            break;
+        case '*':
+            ris = N1 * N2;
+            printf("Risultato: %.2f\n", ris);
+            break;
+        case '/':
+            if (N2 == 0) {
+                printf("Errore: divisione per zero non consentita.\n");
+            } else {
+                ris = (float) N1 / N2;
+                printf("Risultato: %.2f\n", ris);
+            }
             break;
         default:
-            printf("Hai sbagliato qualche cosa");
+            printf("Errore: operatore sconosciuto.\n");
             break;
     }
 
