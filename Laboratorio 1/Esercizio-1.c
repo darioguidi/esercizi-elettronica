c496e6e3045382c6bc9e9e60f53f82ed0a2b46cd
Dario Guidi <dario.guidi954@gmail.com>
2025-03-29 23:03:45 +0100

    .

---
+++ Laboratorio 1/Esercizio-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N1, N2;
+    float risp;
+    char op;
+
+    printf("Inserisci calcolo nella forma <n1> <op> <n2>\n");
+    if(scanf("%d %c %d", N1, op, N2 != 3)){
+        printf("Hai sbagliato ad inserire i valori");
+        return 1;
+    }
+}
