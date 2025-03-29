2538f613bd5e69e8dbcc2730d022bc9bd031a7bd
Dario Guidi <dario.guidi954@gmail.com>
2025-03-29 23:15:10 +0100

    .

--- Laboratorio 1/Esercizio-1.c
+++ Laboratorio 1/Esercizio-1.c
@@ -7,8 +7,20 @@ int main(void)
     char op;
 
     printf("Inserisci calcolo nella forma <n1> <op> <n2>\n");
-    if(scanf("%d %c %d", N1, op, N2 != 3)){
-        printf("Hai sbagliato ad inserire i valori");
+    if(scanf("%d %c %d", N1, op, N2) != 3){
+        printf("Si vede che hai sbagliato qualche cosa");
         return 1;
     }
+
+    switch(op){
+        case '+':
+            risp=N1+N2;
+            printf("Il risultato risulta: %d", risp);
+            break;
+        default:
+            printf("Hai sbagliato qualche cosa");
+            break;
+    }
+
+    return 0;
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# esercizi-elettronica
- 
