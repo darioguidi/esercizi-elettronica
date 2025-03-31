4ec929ba65b2df1f9536e742ee9e745af33fe2ff
Dario Guidi <dario.guidi954@gmail.com>
2025-03-31 20:48:46 +0200

    .

--- Laboratorio 1/esercizio 5/esercizio-5.c
+++ Laboratorio 1/esercizio 5/esercizio-5.c
@@ -2,5 +2,21 @@
 
 int main(void)
 {
-    
+    int num[4];
+
+    printF("Inserisci 4 numeri interi \n");
+
+    for(int i=0; i<4;i++){
+        printf("Inserisci: ");
+        if(num[i]<1000){
+            scanf("%d", num[i]);
+        }else{
+            printf("Numero troppo grande, riporvare \n");
+            i--;
+        }
+    }
+
+
+
+
 }
