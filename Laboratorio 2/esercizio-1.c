9f53fed02e4556a9f29fd319ccce59a5a451e805
Dario Guidi <dario.guidi954@gmail.com>
2025-04-01 23:39:57 +0200

    .

--- Laboratorio 2/esercizio-1.c
+++ Laboratorio 2/esercizio-1.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
+int main(void) {
     char response[8];
     int min = 0, max = 100, guess;
 
-    while(min<max){
+    printf("Pensa a un numero tra 0 e 100.\n");
 
-        guess=(min+max)/2;
+    while (min <= max) { // Corretta la condizione del ciclo
+        guess = (min + max) / 2;
 
-        printf("Il tuo numero è maggiore o minore di %d:  ", guess);
+        printf("Il tuo numero è maggiore, minore o uguale a %d? (maggiore/minore/uguale): ", guess);
+        
+        fgets(response, sizeof(response), stdin);
+        response[strcspn(response, "\n")] = '\0';
 
-        fgets(response,sizeof(response),stdin);
-        response[strcspn(response,"\n")]='\0';
-
-        if(strcmp(response,"maggiore")){
-            min=guess+15;
-        }else if(strcmp(response,"minore")){
-            max=guess-15;
-        } else if (strcmp(response, "uguale") == 0) {
+        if (strcmp(response,"maggiore") == 0) {
+            min = guess + 1; 
+        } else if (strcmp(response,"minore") == 0) {
+            max = guess - 1; 
+        } else if (strcmp(response,"uguale") == 0) {
             printf("Ho indovinato! Il numero è %d.\n", guess);
             return 0;
-        }else{
-            printf("Hai sbagliato a scrivere credo");
+        } else {
+            printf("Hai sbagliato a scrivere.\n");
         }
     }
+
+    printf("Il numero che hai pensato è %d!\n", min);
+    return 0;
 }
